d24cc482f365256719356c7424ac5555d98704ca
a5135324 <a5135324@gmail.com>
2019-09-17 13:09:00 +0800

    init

---
+++ plurk_crawler.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+from plurk_oauth import PlurkAPI
+import requests
+import json
+import ast
+
+class PlurkMain(object):
+    def __init__(self, plurk_id):
+        self.plurk_id = plurk_id
+    
+    def get_information(self, plurk_id):
+        content = plurk.callAPI('/APP/Timeline/getPlurk', options={'plurk_id': plurk_id})
+        #print(json.dumps(content, ensure_ascii=False, indent=4))        
+        print(content["plurk"]["user_id"])
+        print(content["plurk"]["content"])
+        print(content["plurk"]["posted"])
+        print(content["plurk"]["response_count"])
+
+    def add_plurk(self, content, qualifier = 'says'):
+        temp = plurk.callAPI('/APP/Timeline/plurkAdd', options={'content': content, 'qualifier': qualifier})
+        return temp
+
+    def del_plurk(self, plurk_id):
+        temp = plurk.callAPI('/APP/Timeline/plurkDelete', options={'plurk_id': plurk_id})
+        return temp
+
+class PlurkResponse(object):
+    def __init__(self, plurk_id):
+        self.plurk_id = plurk_id
+
+def get_anonymous_plurks(offset = 0, limit = 10):
+    r = requests.get('https://www.plurk.com/Stats/getAnonymousPlurks?lang=zh&offset=' + offset.__str__() + '&limit=' + limit.__str__())
+    plurk_id = json.dumps(json.loads(r.text)["pids"], ensure_ascii=False, indent=4)
+    return plurk_id
+
+def main():
+    global plurk
+    id_list = get_anonymous_plurks(0, 1)
+    ids = ast.literal_eval(id_list)
+    print(ids[0])
+    
+    plurk = PlurkAPI.fromfile('key/API.keys')
+    plurks = PlurkMain(ids[0])
+    plurks.get_information(ids[0])
+
+if __name__ == '__main__':
+    main()
+    
