85593497b2855ba930b6f8e56b33142d11e63121
a5135324 <a5135324@gmail.com>
2019-09-17 13:36:03 +0800

    update

--- plurk_crawler.py
+++ plurk_crawler.py
@@ -16,6 +16,15 @@ class PlurkMain(object):
         print(content["plurk"]["posted"])
         print(content["plurk"]["response_count"])
 
+class PlurkResponse(object):
+    def __init__(self, plurk_id):
+        self.plurk_id = plurk_id
+
+class PlurkUser(object):
+    def __init__(self):
+        temp = plurk.callAPI('/APP/Users/me')
+        self.user_id = temp["id"]
+
     def add_plurk(self, content, qualifier = 'says'):
         temp = plurk.callAPI('/APP/Timeline/plurkAdd', options={'content': content, 'qualifier': qualifier})
         return temp
@@ -23,10 +32,6 @@ class PlurkMain(object):
     def del_plurk(self, plurk_id):
         temp = plurk.callAPI('/APP/Timeline/plurkDelete', options={'plurk_id': plurk_id})
         return temp
-
-class PlurkResponse(object):
-    def __init__(self, plurk_id):
-        self.plurk_id = plurk_id
 
 def get_anonymous_plurks(offset = 0, limit = 10):
     r = requests.get('https://www.plurk.com/Stats/getAnonymousPlurks?lang=zh&offset=' + offset.__str__() + '&limit=' + limit.__str__())
@@ -38,11 +43,12 @@ def main():
     id_list = get_anonymous_plurks(0, 1)
     ids = ast.literal_eval(id_list)
     print(ids[0])
-    
+
     plurk = PlurkAPI.fromfile('key/API.keys')
+    author = PlurkUser()
+    author.add_plurk('Post By API')
     plurks = PlurkMain(ids[0])
     plurks.get_information(ids[0])
 
 if __name__ == '__main__':
     main()
-    
