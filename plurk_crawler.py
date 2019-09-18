5c929dfb7ab92db955e4346092f0ac534d77bd87
a5135324 <a5135324@gmail.com>
2019-09-19 01:04:09 +0800

    add movie crawler

--- plurk_crawler.py
+++ plurk_crawler.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 from plurk_oauth import PlurkAPI
+from bs4 import BeautifulSoup
+import pandas as pd
 import requests
 import json
 import ast
@@ -25,7 +27,7 @@ class PlurkUser(object):
         temp = plurk.callAPI('/APP/Users/me')
         self.user_id = temp["id"]
 
-    def add_plurk(self, content, qualifier = 'says'):
+    def add_plurk(self, content, qualifier = ''):
         temp = plurk.callAPI('/APP/Timeline/plurkAdd', options={'content': content, 'qualifier': qualifier})
         return temp
 
@@ -33,22 +35,74 @@ class PlurkUser(object):
         temp = plurk.callAPI('/APP/Timeline/plurkDelete', options={'plurk_id': plurk_id})
         return temp
 
+    def add_response(self, plurk_id, content, qualifier = ''):
+        temp = plurk.callAPI('/APP/Responses/responseAdd', options={'plurk_id': plurk_id, 'content': content, 'qualifier': qualifier})
+        return temp
+
 def get_anonymous_plurks(offset = 0, limit = 10):
     r = requests.get('https://www.plurk.com/Stats/getAnonymousPlurks?lang=zh&offset=' + offset.__str__() + '&limit=' + limit.__str__())
     plurk_id = json.dumps(json.loads(r.text)["pids"], ensure_ascii=False, indent=4)
     return plurk_id
 
+def yahoo_movie_parser():
+    url = 'https://movies.yahoo.com.tw/movie_thisweek.html'
+    r = requests.get(url)
+    web_content = r.text
+    soup = BeautifulSoup(web_content,'html.parser')
+    newMovie = soup.find_all('div', class_= "release_movie_name")
+    #print(newMovie)
+    NameCHs = [t.find('a', class_='gabtn').text.replace('\n','').replace(' ','') for t in newMovie]
+    #print(NameCHs)
+    NameENs = [t.find('div', class_='en').find('a').text.replace('\n','').replace(' ','', 20) for t in newMovie]
+    #print(NameENs)
+    newMovie3 = soup.find_all('div',class_="release_btn color_btnbox")
+    #print(newMovie3)
+    links = []
+    for t in newMovie3:
+        if t.find('a', class_ = "btn_s_vedio gabtn"):
+            links.append(t.find('a',class_="btn_s_vedio gabtn")['href'])
+        else:
+            links.append('')
+    #print(links)
+    newMovie4 = soup.find_all('div', class_ = 'release_info_text')
+    #print(newMovie4)
+    release_time = [t.find('div', class_ = 'release_movie_time').text for t in newMovie4]
+    #print(release_time)
+    '''
+    newMovie4 = soup.find_all('div',class_="release_text")
+    #print(newMovie4)
+    Intros = [t.find('span').text.replace('\n','').replace('\r','').replace('\xa0','').replace(' ','') for t in newMovie4]
+    #print(Intros)
+    '''
+    df = pd.DataFrame(
+    {
+        'Name':NameCHs,
+        'EnName':NameENs,
+        'ReTime': release_time,
+        'Trailer': links
+    })
+    print(df['Trailer'])
+    
+
 def main():
     global plurk
+    '''
     id_list = get_anonymous_plurks(0, 1)
     ids = ast.literal_eval(id_list)
     print(ids[0])
+    plurks = PlurkMain(ids[0])
+    plurks.get_information(ids[0])
+    '''
 
     plurk = PlurkAPI.fromfile('key/API.keys')
     author = PlurkUser()
-    author.add_plurk('Post By API')
-    plurks = PlurkMain(ids[0])
-    plurks.get_information(ids[0])
+    #temp = author.add_plurk('本週上映電影')
+    #print(temp['plurk_id'])
+    # 1420558413
+    plurk.callAPI('/APP/Responses/responseAdd', options={'plurk_id': '1420558413', 'content': 'test\ntest', 'qualifier':''})
+
+    
+    #yahoo_movie_parser()
 
 if __name__ == '__main__':
     main()
